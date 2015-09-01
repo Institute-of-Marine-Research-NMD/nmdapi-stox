ea2441bf7708b0b1c707e852ba25ece0048798f6
KjetilFjellheim <kjetil.fjellheim@imr.no>
2015-09-01 16:25:01 +0200

    Refactored and added new dao for reference and series

--- service/src/main/java/no/imr/nmdapi/stox/service/NMDStoxServiceImpl.java
+++ service/src/main/java/no/imr/nmdapi/stox/service/NMDStoxServiceImpl.java
@@ -2,9 +2,10 @@ package no.imr.nmdapi.stox.service;
 
 import java.util.List;
 import no.imr.nmd.commons.stox.jaxb.v1.StoxProjectType;
-import no.imr.nmdapi.dao.file.NMDDataDao;
+import no.imr.nmdapi.dao.file.NMDSeriesReferenceDao;
 import no.imr.nmdapi.generic.response.v1.ListElementType;
 import no.imr.nmdapi.generic.response.v1.ResultElementType;
+import org.apache.commons.configuration.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -13,33 +14,43 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author kjetilf
  */
 public class NMDStoxServiceImpl implements NMDStoxService {
+    /**
+     * Data type.
+     */
+    private static final String TYPE = "stox";
 
     @Autowired
-    private NMDDataDao nmdDataDao;
+    private NMDSeriesReferenceDao seriesReferenceDao;
 
-    @Override
+    @Autowired
+    private Configuration configuration;
+
+     @Override
     public Object getData(final String name) {
-        return nmdDataDao.get(name, StoxProjectType.class);
+        return seriesReferenceDao.get(name, StoxProjectType.class.getPackage().getName());
     }
 
     @Override
     public void deleteData(final String name) {
-        nmdDataDao.delete(name);
+        seriesReferenceDao.delete(TYPE, name, true);
     }
 
    @Override
-    public void insertData(final String name, final StoxProjectType projectType) {
-        nmdDataDao.insert(name, projectType, StoxProjectType.class);
+    public void insertData(final String name, final StoxProjectType stoxProjectType) {
+        String readRole = configuration.getString("default.readrole");
+        String writeRole = configuration.getString("default.writerole");
+        String owner = configuration.getString("default.owner");
+        seriesReferenceDao.insert(writeRole, readRole, owner, TYPE, name, stoxProjectType, true);
     }
 
     @Override
-    public void updateData(final String name, final StoxProjectType projectType) {
-        nmdDataDao.update(name, projectType, StoxProjectType.class);
+    public void updateData(final String name, final StoxProjectType stoxProjectType) {
+        seriesReferenceDao.update(name, stoxProjectType);
     }
 
     @Override
     public ListElementType list() {
-        List<String> names = nmdDataDao.listSeries();
+        List<String> names = seriesReferenceDao.list();
         ListElementType elementType = new ListElementType();
         for (String name : names) {
             ResultElementType resultElementType = new ResultElementType();
